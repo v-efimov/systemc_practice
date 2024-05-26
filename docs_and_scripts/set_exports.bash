0c46bb424b52d5d47cc69a5c248b17684a50200c
va <Vladimir Efimov>
2024-05-27 00:53:49 +0300

    Added docs_and_scripts directory

---
+++ docs_and_scripts/set_exports.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# RUN THIS AS source ./set_exports.bash #
+#These are required to build Verilator and SystemC and run Verilator together with SystemC
+export CXX="g++"
+export CXXFLAGS="-std=c++14"
+export LD_LIBRARY_PATH=`pwd`"/systemc-2.3.3/lib-linux64"
+export SYSTEMC_INCLUDE=`pwd`"/systemc-2.3.3/include"
+export SYSTEMC_LIBDIR=`pwd`"/systemc-2.3.3/lib-linux64"
+export VERILATOR_ROOT=`pwd`"/verilator"
+#These are required to run SystemC examples without Verilator
+export SYSTEMC_HOME=`pwd`"/systemc-2.3.3"
+export TARGET_ARCH="linux64"
+
