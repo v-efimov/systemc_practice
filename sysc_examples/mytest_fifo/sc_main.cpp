c9714d08cfb1dce67115624ec791c9bc4a0c7a31
vladimire@etherblade.net <vladimire@etherblade.net>
2024-05-28 20:02:26 +0300

    Added systemc tutorial examples

---
+++ sysc_examples/mytest_fifo/sc_main.cpp
@@ -0,0 +1,87 @@
+// SystemC global header
+#include <systemc.h>
+
+using namespace sc_core;
+
+
+SC_MODULE (FIFO) {
+  sc_fifo<int> f1, f2, f3;
+  SC_CTOR(FIFO) : f1(2), f2(2), f3(2) {
+    SC_THREAD(generator1);
+    SC_THREAD(consumer1);
+    SC_THREAD(generator2);
+    SC_THREAD(consumer2);
+    SC_THREAD(generator3);
+    SC_THREAD(consumer3);
+  }
+
+
+void generator1() {
+  int v = 0;
+  while (true) {
+    f1.write(v);
+    std::cout << sc_time_stamp() << ": generator1 writes " << v++ << std::endl;
+    wait(1, SC_SEC);
+   }
+ }
+
+
+void consumer1() {
+  int v = -1;
+  while (true) {
+    f1.read(v);
+       std::cout << sc_time_stamp() << ": consumer1 reads " << v << std::endl;
+       wait(3, SC_SEC);
+  }
+}
+
+void generator2() {
+  int v = 0;
+  while (true) {
+    while (f2.nb_write(v) == false) {
+      wait(f2.data_read_event());
+  }
+  std::cout << sc_time_stamp() << ": generator2 writes " << v++ << std::endl;
+  wait(1, SC_SEC);
+  }
+}
+
+void consumer2() {
+  int v = -1;
+  while (true) {
+    while (f2.nb_read(v) == false) {
+      wait(f2.data_written_event());
+   }
+   std::cout << sc_time_stamp() << ": consumer2 reads " << v << std::endl;
+   wait(3, SC_SEC);
+   }
+}
+
+void generator3() {
+  int v = 0;
+  while (true) {
+    std::cout << sc_time_stamp() << ": generator3, before write, #free/#available=" << f3.num_free() << "/" << f3.num_available() << std::endl;
+    f3.write(v++);
+    std::cout << sc_time_stamp() << ": generator3, after write, #free/#available=" << f3.num_free() << "/" << f3.num_available() << std::endl;
+    wait(1, SC_SEC);
+    }
+}
+
+void consumer3() {
+   int v = -1;
+  while (true) {
+    std::cout << sc_time_stamp() << ": consumer3, before read, #free/#available=" << f3.num_free() << "/" << f3.num_available() << std::endl;
+    f3.read(v);
+    std::cout << sc_time_stamp() << ": consumer3, after read, #free/#availalble=" << f3.num_free() << "/" << f3.num_available() << std::endl;
+    wait(3, SC_SEC);
+    }
+  }
+
+};
+
+
+int sc_main(int, char* []) {
+    FIFO fifo("fifo");
+    sc_start(10, SC_SEC);
+    return 0;
+}
---
+++ sysc_examples/mytest_my_first_rob/sc_main.cpp
@@ -0,0 +1,59 @@
+// SystemC global header
+#include <systemc.h>
+#include <iostream>
+
+using namespace sc_core;
+
+
+SC_MODULE (MUTEX) {
+  sc_mutex m;
+  SC_CTOR(MUTEX) {
+    SC_THREAD(thread_1);
+    SC_THREAD(thread_2);
+  }
+
+
+void thread_1() {
+  while (true) {
+    if (m.trylock() == -1) {
+      m.lock();
+      std::cout << sc_time_stamp() << ": thread_1 obtained resource by lock()" << std::endl;
+      cin.get();
+                           }
+    else {
+      std::cout << sc_time_stamp() << ": thread_1 obtained resource by trylock()" << std::endl;
+      cin.get();
+         }
+    wait(1, SC_SEC);
+    m.unlock();
+    std::cout << sc_time_stamp() << ": unlocked by thread_1" << std::endl; 
+    wait(SC_ZERO_TIME);
+   }
+ }
+
+
+void thread_2 () {
+   while (true) {
+     if (m.trylock() == -1) {
+       m.lock();
+       std::cout << sc_time_stamp() << ": thread_2 obtained resource by lock()" << std::endl; 
+       cin.get();
+                            }
+     else {
+       std::cout << sc_time_stamp() << ": thread_2 obtained resource by trylock()" << std::endl; 
+       cin.get();
+          }
+     wait(1, SC_SEC);
+     m.unlock();
+     std::cout << sc_time_stamp() << ": unlocked by thread_2" << std:: endl;
+     wait(SC_ZERO_TIME);
+    }
+  }
+};
+
+
+int sc_main(int, char* []) {
+    MUTEX mutex("mutex");
+    sc_start(4, SC_SEC);
+    return 0;
+}
