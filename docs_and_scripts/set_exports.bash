6ed033170f596e16418ed6a38ebfdfc2b8db632b
v-efimov <53142676+v-efimov@users.noreply.github.com>
2024-11-04 19:53:51 +0300

    Changes in export variables that allow explicitly set g++ version (14, 17 etc)

--- docs_and_scripts/set_exports.bash
+++ docs_and_scripts/set_exports.bash
@@ -2,8 +2,10 @@
 
 # RUN THIS AS source ./set_exports.bash #
 #These are required to build Verilator and SystemC and run Verilator together with SystemC
+#If necessary change c++14/gnu++14 to c++17/gnu++17
 export CXX="g++"
 export CXXFLAGS="-std=c++14"
+export CFG_CXXFLAGS_STD_NEWEST="-std=gnu++14"
 export LD_LIBRARY_PATH=`pwd`"/systemc-2.3.3/lib-linux64"
 export SYSTEMC_INCLUDE=`pwd`"/systemc-2.3.3/include"
 export SYSTEMC_LIBDIR=`pwd`"/systemc-2.3.3/lib-linux64"
