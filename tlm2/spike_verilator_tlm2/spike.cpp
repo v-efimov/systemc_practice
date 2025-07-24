a83d44c3185b6702ba370aa2f650a7af8161f26f
v-efimov <53142676+v-efimov@users.noreply.github.com>
2025-07-24 19:52:37 +0200

    Added exit condition to spike module

--- tlm2/spike_verilator_tlm2/spike.cpp
+++ tlm2/spike_verilator_tlm2/spike.cpp
@@ -8,18 +8,19 @@ void spike_module::start_of_simulation() {
 
 void spike_module::spike_thread() {
 
+  bool keep_sim = true;
   bool input_sanity_check_ok = false;
   int inputaddress;
   int inputdata;
   int inputoperation;
 
-  while (true) {
+  while (keep_sim) {
 
     input_sanity_check_ok = false;
     do {
-        std::cout << "Curr simtime:" <<  sc_time_stamp() << ", provide \"ADDRESS    DATA    OPERATION(1-write/2-read)\" separated by spaces: ";
+        std::cout << "Curr simtime:" <<  sc_time_stamp() << ", provide \"ADDRESS    DATA    OPERATION(0-exit/1-write/2-read)\" separated by spaces: ";
         std::cin >> inputaddress >> inputdata >> inputoperation;
-        if (!std::cin.fail() && inputoperation >= 1 && inputoperation <= 2) {
+        if (!std::cin.fail() && inputoperation >= 0 && inputoperation <= 2) {
             input_sanity_check_ok = true;
             std::cout << "Input accepted" << std::endl;
         } else {
@@ -29,30 +30,34 @@ void spike_module::spike_thread() {
         }
     } while (!input_sanity_check_ok);
 
-    if (inputoperation == 1) {
-        spike_uncore_trans->set_command(tlm::TLM_WRITE_COMMAND);
+    if (inputoperation == 0) {
+        stopsim_port->write(true);   //request to finish simulation
+        keep_sim = false;
     } else {
-        spike_uncore_trans->set_command(tlm::TLM_READ_COMMAND);
+        if (inputoperation == 1) {
+            spike_uncore_trans->set_command(tlm::TLM_WRITE_COMMAND);
+        } else {
+            spike_uncore_trans->set_command(tlm::TLM_READ_COMMAND);
+        }
+        spike_uncore_trans->set_address(inputaddress);
+        data_array[0] = inputdata;
+        spike_uncore_trans->set_data_ptr( reinterpret_cast<unsigned char*>(data_array) );
+        spike_uncore_trans->set_data_length(sizeof(data_array));
+        spike_uncore_trans->set_streaming_width(sizeof(data_array));
+        spike_uncore_trans->set_byte_enable_ptr(0);
+        spike_uncore_trans->set_dmi_allowed(false);
+        spike_uncore_trans->set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+
+        model_delay = sc_time(10, SC_NS);
+        spike_uncore_socket->b_transport(*spike_uncore_trans, model_delay);
+
+        if ( spike_uncore_trans->get_response_status() != tlm::TLM_OK_RESPONSE ) {
+            cout << "Transaction returned with error: " << spike_uncore_trans->get_response_status() << endl;
+        } else {
+            cout << "B_Transport_response_OK" << endl;
+        }
+
+        cout << "RESP_DATA: " << data_array[0] << endl;
     }
-    spike_uncore_trans->set_address(inputaddress);
-    data_array[0] = inputdata;
-    spike_uncore_trans->set_data_ptr( reinterpret_cast<unsigned char*>(data_array) );
-    spike_uncore_trans->set_data_length(sizeof(data_array));
-    spike_uncore_trans->set_streaming_width(sizeof(data_array));
-    spike_uncore_trans->set_byte_enable_ptr(0);
-    spike_uncore_trans->set_dmi_allowed(false);
-    spike_uncore_trans->set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
-
-    model_delay = sc_time(10, SC_NS);
-    spike_uncore_socket->b_transport(*spike_uncore_trans, model_delay);
-
-    if ( spike_uncore_trans->get_response_status() != tlm::TLM_OK_RESPONSE ) {
-        cout << "Transaction returned with error: " << spike_uncore_trans->get_response_status() << endl;
-    } else {
-        cout << "B_Transport_response_OK" << endl;
-    }
-
-    cout << "RESP_DATA: " << data_array[0] << endl;
-
   }
 }
